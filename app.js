7dcf8b5eaca982605be35312592a71dcd60500d0
Rosa Kang <rosakang12@gmail.com>
2018-08-01 23:31:16 -0600

    app.js changes

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 /*
@@ -21,16 +10,16 @@ init ();
 
 
 //setter
-//document.querySelector('#current-' + activePlayer).textContent = dice; //# for picking ids - selecting current score // using text content method to change text
+//document.querySelector('#current-' + activePlayer).textContent = card; //# for picking ids - selecting current score // using text content method to change text
 
 //other method:
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + card + '</em>';
 
 //getter
 //var x = document.querySelector('#score-0').textContent; //to read element id score-0
 //console.log(x);
 
-//document.querySelector('.dice').style.display = 'none'; //.style.css = value ---> this is to hide the dice 
+//document.querySelector('.card').style.display = 'none'; //.style.css = value ---> this is to hide the card 
 
 //when working with ID's
 //setting all scores to 0
@@ -72,6 +61,10 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         else {
             //next player
             nextPlayer();
+            
+            
+            //option of choosing for a 1 or 11
+            
         }
     }
 });
@@ -94,6 +87,14 @@ document.querySelector('.btn-hold').addEventListener('click',function() {
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         }
+        
+        else if (scores[activePlayer] > 21 || roundScore > 21){
+            document.querySelector('#name-'+activePlayer).textContent = 'Loser!';
+            document.querySelector('.card').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('loser');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        }
         else {
         //next player
         nextPlayer();
@@ -105,7 +106,7 @@ function nextPlayer() {
     //next player
     //ternary operator 
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
+    //roundScore = 0;
         
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -117,7 +118,7 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active'); 
         
-    //making dice disappear with a 1
+    //making card disappear with a 1
     document.querySelector('.card').style.display = 'none';
 }
 
